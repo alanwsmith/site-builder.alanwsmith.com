f39d3fb0e7adebe881a47b475ebff79777ca1937
Alan Smith <commit@alanwsmith.com>
2025-01-17 22:36:32 -0500

    Including files by lines

--- src/page/mod.rs
+++ src/page/mod.rs
@@ -13,9 +13,11 @@ impl Page {
     pub fn new(content: String) -> Page {
         Page { content }
     }
+
     pub fn ping(&self) -> Result<Value, Error> {
         Ok(Value::from("this is stuff"))
     }
+
     pub fn include_file(&self, args: &[Value]) -> Result<Value, Error> {
         // TODO: Fix so you can't do `.../` etc...
         if args.len() == 1 {
@@ -25,6 +27,30 @@ impl Page {
             Ok(Value::from(content))
         } else {
             Ok(Value::from("could not file file"))
+        }
+    }
+
+    pub fn include_file_lines(&self, args: &[Value]) -> Result<Value, Error> {
+        // TODO: Fix so you can't do `.../` etc...
+        if args.len() == 3 {
+            let content_dir = PathBuf::from("content");
+            let file_path = content_dir.join(args[0].to_string());
+            let content = fs::read_to_string(file_path).unwrap();
+            let lines = content.lines();
+            let start_line: usize = args[1].to_string().parse().unwrap();
+            let end_line: usize = args[2].to_string().parse().unwrap();
+            let output = lines
+                .into_iter()
+                .skip(start_line)
+                .take(end_line - start_line + 1)
+                .map(|l| l.to_string())
+                .collect::<Vec<String>>()
+                .join("\n");
+            Ok(Value::from(output))
+        } else {
+            Ok(Value::from(
+                "could not file file, or args problem with not enough args",
+            ))
         }
     }
 }
@@ -39,6 +65,7 @@ impl Object for Page {
         match name {
             "ping" => self.ping(),
             "include_file" => self.include_file(args),
+            "include_file_lines" => self.include_file_lines(args),
             _ => Ok(Value::from("")),
         }
     }
