ec765138f888deee46c9f21339a83ab602137274
Alan Smith <commit@alanwsmith.com>
2025-01-17 21:37:55 -0500

    starting to add minijinja for templating

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use minijinja::syntax::SyntaxConfig;
+use minijinja::{context, Environment};
 use std::fs;
 use std::path::PathBuf;
 use walkdir::{DirEntry, WalkDir};
@@ -35,12 +37,55 @@ fn get_source_files() -> Vec<PathBuf> {
 }
 
 fn process_source_files(files: Vec<PathBuf>) {
-    let input_dir = PathBuf::from("content");
+    let mut env = Environment::new();
+    env.set_syntax(
+        SyntaxConfig::builder()
+            .block_delimiters("[!", "!]")
+            .variable_delimiters("[@", "@]")
+            .comment_delimiters("[#", "#]")
+            .build()
+            .unwrap(),
+    );
+
+    let wrapper_path = "templates/wrappers/main.html";
+    let wrapper_string = fs::read_to_string(&wrapper_path).unwrap();
+    env.add_template_owned(wrapper_path, wrapper_string)
+        .unwrap();
+
+    let content_dir = PathBuf::from("content");
     let output_dir = PathBuf::from("_site");
     files.iter().for_each(|file| {
-        let input_path = input_dir.join(file);
+        let input_path = content_dir.join(file);
         let output_path = output_dir.join(file);
         let content = fs::read_to_string(input_path).unwrap();
-        fs::write(output_path, content).unwrap();
+        env.add_template_owned("working-file", content).unwrap();
+        match env.get_template("working-file") {
+            Ok(template) => match template.render(context!()) {
+                Ok(output) => {
+                    write_file_with_mkdir(&output_path, &output).unwrap();
+                }
+                Err(e) => {
+                    dbg!(e);
+                    ()
+                }
+            },
+            Err(e) => {
+                dbg!(e);
+                ()
+            }
+        }
     })
+}
+
+fn write_file_with_mkdir(path: &PathBuf, content: &str) -> Result<(), String> {
+    match path.parent() {
+        Some(parent_dir) => match fs::create_dir_all(parent_dir) {
+            Ok(_) => match fs::write(path, content) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e.to_string()),
+            },
+            Err(e) => Err(e.to_string()),
+        },
+        None => Err("Could not make directory".to_string()),
+    }
 }
